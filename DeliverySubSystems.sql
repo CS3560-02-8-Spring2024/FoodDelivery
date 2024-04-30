c5285e5be2771af642a45f29cb9ab2de6f4fcb54
JellyFB <131621190+JellyFB@users.noreply.github.com>
2024-04-29 23:03:32 -0700

    Updating the database creation
    reordered the tables to allow for foreign keys to work as well as referencing them so the tables can be initialized
    (may change the database name in the future

--- DeliverySubSystems.sql
+++ DeliverySubSystems.sql
@@ -1,54 +1,59 @@
-Create Table 'menu'(
-	'item_id' int(10) Not Null,
-    'foodName' varchar(20) Not Null,
-    'price' decimal(5,2) Not Null,
-    Primary Key('item_id')
-)
-Create Table 'orderItem'(
-	'orderitem_id' int(10) Not Null,
-    'quantity' int(10) Not Null,
-    'item_id' int(10) Not Null,
-    'order_id' int(10) Not Null,
-    Primary Key('orderitem_id')
-    Foreign Key('item_id')
-    Foreign Key('order_id')
-)
-Create Table 'order'(
-	'order_id' int(10) Not Null,
-    'customer_id' int(10) Not Null,
-    'driver_id' int(10) Not Null,
-    'staff_id' int(10) Not Null,
-    'deliveryStatus' char(12) Not Null,
-    Primary Key('order_id')
-    Foreign Key('customer_id')
-    Foreign Key('driver_id')
-    Foreign Key('staff_id')
-)
-Create Table 'staff'(
-	'staff_id' int(10) Not Null,
-    'staffName' varchar(30) Not Null,
-    Primary Key('staff_id')
-)
-Create Table 'deliverDriver'(
-	'driver_id' int(10) Not Null,
-    'availability' varchar(3) Not Null,
-    Primary Key('driver_id')
-)
-Create Table 'customer'(
-	'customer_id' int(10) Not Null,
-    'address' varchar(30) Not Null,
-    'phoneNumber' varchar(10) Not Null,
-    'paymentInfo' varchar(30) Not Null,
-    Primary Key ('customer_id')
-)
-Create Table 'review'(
-	'review_id' int(10) Not Null,
-    'customer_id' int(10) Not Null,
-    'foodQuality' int(5) Not Null,
-    'serviceQuality' int(5) Not Null,
-    'deliveryQuality' int(5) Not Null,
-    'date' varchar(8) Not Null,
-    'otherComments' varchar(500) Not Null,
-    Primary Key('review_id')
-    Foreign Key('customer_id')
-)
+/**INITIALIZING A DATABASE AND USING IT TO CREATE THE NECESSARY TABLES**/
+CREATE DATABASE CS3560DFSS;
+
+USE CS3560DFSS;
+
+CREATE TABLE menu(
+	item_id int(10) NOT NULL,
+    foodName varchar(20) NOT NULL,
+    price decimal(5,2) NOT NULL,
+    PRIMARY KEY(item_id)
+);
+CREATE TABLE staff(
+	staff_id int(10) NOT NULL,
+    staffName varchar(30) NOT NULL,
+    PRIMARY KEY(staff_id)
+);
+CREATE TABLE deliverDriver(
+	driver_id int(10) NOT NULL,
+    availability varchar(3) NOT NULL,
+    PRIMARY KEY(driver_id)
+);
+CREATE TABLE customer(
+	customer_id int(10) NOT NULL,
+    address varchar(30) NOT NULL,
+    phoneNumber varchar(10) NOT NULL,
+    paymentInfo varchar(30) NOT NULL,
+    PRIMARY KEY(customer_id)
+);
+CREATE TABLE _order(
+	order_id int(10) NOT NULL,
+    customer_id int(10) NOT NULL,
+    driver_id int(10) NOT NULL,
+    staff_id int(10) NOT NULL,
+    deliveryStatus char(12) NOT NULL,
+    PRIMARY KEY(order_id),
+    FOREIGN KEY(customer_id) REFERENCES customer(customer_id),
+    FOREIGN KEY(driver_id) REFERENCES deliverDriver(driver_id),
+    FOREIGN KEY(staff_id) REFERENCES staff(staff_id)
+);
+CREATE TABLE orderItem(
+	orderitem_id int(10) NOT NULL,
+    quantity int(10) NOT NULL,
+    item_id int(10) NOT NULL,
+    order_id int(10) NOT NULL,
+    PRIMARY KEY(orderitem_id),
+    FOREIGN KEY(item_id) REFERENCES menu(item_id),
+    FOREIGN KEY(order_id) REFERENCES _order(order_id)
+);
+CREATE TABLE review(
+	review_id int(10) NOT NULL,
+    customer_id int(10) NOT NULL,
+    foodQuality int(5) NOT NULL,
+    serviceQuality int(5) NOT NULL,
+    deliveryQuality int(5) NOT NULL,
+    _date varchar(8) NOT NULL,
+    otherComments varchar(500) NOT NULL,
+    PRIMARY KEY(review_id),
+    FOREIGN KEY(customer_id) REFERENCES customer(customer_id)
+);
