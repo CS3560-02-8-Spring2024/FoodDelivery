7c61f0756369b3aa4628009c691ccdbb448b380e
Hans <125611985+DimeMayo@users.noreply.github.com>
2024-05-06 17:43:01 -0700

    Update SampleData.sql
    added tables for order, menu, orderItem, review, and address

--- Demo/SampleData.sql
+++ Demo/SampleData.sql
@@ -29,5 +29,58 @@ CREATE TABLE staff(
     PRIMARY KEY(staff_id)
 );
 
+CREATE TABLE _order (
+	order_id INT(4) NOT NULL AUTO_INCREMENT,
+    customer_id int(10),
+    staff_id int(10),
+    driver_id int(10),
+    deliveryStatus ENUM('Ordered','Ready_for_delivery','Picked_up','Delivered') DEFAULT 'Ordered',
+    FOREIGN KEY(customer_id) REFERENCES customer(customer_id),
+    FOREIGN KEY(staff_id) REFERENCES staff(staff_id),
+    FOREIGN KEY(driver_id) REFERENCES customer(customer_id),
+    PRIMARY KEY(staff_id)
+);
+
+CREATE TABLE menu (
+	item_id INT(10) NOT NULL,
+    foodName VARCHAR(20),
+    price DECIMAL(5,2),
+    _description VARCHAR(255),
+    PRIMARY KEY(staff_id)
+);
+
+CREATE TABLE orderItem(
+	orderItem_id INT(10),
+    quantity INT(3),
+    item_id INT(10),
+    order_id INT(4),
+    FOREIGN KEY(item_id) REFERENCES menu(item_id),
+    FOREIGN KEY( order_id) REFERENCES _order(order_id),
+    PRIMARY KEY(orderItem_id)
+);
+
+CREATE TABLE review(
+	review_id INT(10),
+    foodQuality INT(1),
+    serviceQuality INT(1),
+    deliveryQuality INT(1),
+    _date VARCHAR(8),
+    otherComments VARCHAR(500),
+    customer_id INT(10),
+    FOREIGN KEY(customer_id) REFERENCES customer(customer_id),
+    PRIMARY KEY(review_id)
+);
+
+CREATE TABLE address(
+	address_id INT(10),
+    street VARCHAR(50),
+    city VARCHAR(20),
+    state VARCHAR(5),
+    zipCode INT(5),
+    customer_id INT(10),
+    FOREIGN KEY(customer_id) REFERENCES customer(customer_id),
+    PRIMARY KEY(address_id)
+);
+
 INSERT INTO `fdss`.`staff` (`staff_id`, `emailAddres`, `_password`, `firstName`, `lastName`) VALUES ('1', 'staff@cpp.edu', 'staff', 'Vincent', 'La');
 INSERT INTO `fdss`.`driver`(`driver_id`, `emailAddres`, `_password`, `availability`) VALUES ('1', 'bob@cpp.edu', 'builder');
